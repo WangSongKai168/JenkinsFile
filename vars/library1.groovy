d883ef974434f544709df2e4aa21b88082162d0e
WangSongKai168 <139533224+WangSongKai168@users.noreply.github.com>
2023-09-12 09:15:05 +0800

    Update library1.groovy

--- vars/library1.groovy
+++ vars/library1.groovy
@@ -1,60 +1,56 @@
-def call(Map pipelineParams) {
-pipeline {
-    agent any
-    stages {
-        stage ("Stage1: Request website") {
-            steps{
-                script{
-                    response_code = sh (
-                        script: "curl -o /dev/null -s -w %{http_code} www.example.com",
-                        returnStdout: true
-                    ).toInteger()
-                    
-                    echo "${response_code}"                    
-                }
-            }
-        }
-        stage ("Stage2: Check response code") {
-            steps{
-                script{
-                    if ( response_code != 200 ){
-                        sh "false"
-                    }             
-                }
-
-            }
-        }
-    }
-    post {
-        success{
-            echo "success"
-            script {
-                withCredentials([string(credentialsId: 'TOKEN', variable: 'TOKEN')]){
-                    withCredentials([string(credentialsId: 'GROUP_ID', variable: 'GROUP_ID')]){
-                        url = 'http://aspnetcore/api/Alert/AlertWebhook'
-                        jsonBody = '{"StatusCode": "SUCCESS"}'
-                        sh '''
-                            curl -X POST -H 'Content-Type: application/json' -d '${jsonBody}' ${url}
-                        '''
+def call(Map args) {
+    pipeline {
+        agent any
+        stages {
+            stage ("Stage1: Request website") {
+                steps{
+                    script{
+                        response_code = sh (
+                            script: "curl -o /dev/null -s -w %{http_code} ${args.domain}",
+                            returnStdout: true
+                        ).toInteger()
+                        
+                        echo "${response_code}"                    
                     }
                 }
             }
-        }
-        failure{
-            echo "failure"          
-            script {
-                withCredentials([string(credentialsId: 'TOKEN', variable: 'TOKEN')]){
-                    withCredentials([string(credentialsId: 'GROUP_ID', variable: 'GROUP_ID')]){
-                        url = 'http://aspnetcore/api/Alert/AlertWebhook'
-                        jsonBody = '{"StatusCode": "FAILURE"}'
-                        sh '''
-                            curl -X POST -H 'Content-Type: application/json' -d '${jsonBody}' ${url}
-                        '''
+            stage ("Stage2: Check response code") {
+                steps{
+                    script{
+                        if ( response_code != ${args.statuscode} ){
+                            sh "false"
+                        }             
                     }
+    
                 }
             }
         }
-        
+        post {
+            success{
+                echo "success"
+                script {
+                    withCredentials([string(credentialsId: 'TOKEN', variable: 'TOKEN')]){
+                        withCredentials([string(credentialsId: 'GROUP_ID', variable: 'GROUP_ID')]){
+                            sh '''
+                                curl -X POST -H 'Content-Type: application/json' -d '{"Domain": "${args.domain}", "StatusCode": "SUCCESS"}' http://aspnetcore/api/Alert/AlertWebhook
+                            '''
+                        }
+                    }
+                }
+            }
+            failure{
+                echo "failure"          
+                script {
+                    withCredentials([string(credentialsId: 'TOKEN', variable: 'TOKEN')]){
+                        withCredentials([string(credentialsId: 'GROUP_ID', variable: 'GROUP_ID')]){
+                            sh '''
+                                curl -X POST -H 'Content-Type: application/json' -d '{"Domain": "${args.domain}", "StatusCode": "FAILURE"}' http://aspnetcore/api/Alert/AlertWebhook
+                            '''
+                        }
+                    }
+                }
+            }
+            
+        }
     }
-}
 }
